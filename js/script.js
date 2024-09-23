2691eccfea9633b1bdff37fd466a48761eb22f7b
Daffa Ariyuanda <daffa.ariyuanda@gmail.com>
2024-09-23 13:28:58 +0700

    konfigurasi awal mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-// Ini Javascript
+// Fungsi untuk menampilkan nama pengguna
+function displayUsername() {
+    var username = "John Doe"; // Ganti dengan data dinamis
+    document.getElementById("greeting").innerHTML = "Hi " + username + ", Welcome to My Website";
+}
+
+
+
+// Fungsi validasi dan pengiriman form
+function submitForm() {
+    const name = document.getElementById("name").value;
+    const dob = document.getElementById("dob").value;
+    const gender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : '';
+    const message = document.getElementById("message").value;
+
+    document.getElementById("displayName").textContent = name;
+    document.getElementById("displayDob").textContent = dob;
+    document.getElementById("displayGender").textContent = gender;
+    document.getElementById("displayMessage").textContent = message;
+
+    alert("Form berhasil disubmit!");
+}
